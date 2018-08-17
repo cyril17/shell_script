faa24422b905b23bf4e5f35c2aa97774bf961770
cyril <36559067@qq.com>
2018-08-18 01:59:43 +0800

    centos auto install python

---
+++ create_yum/install_yum.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+echo -e '\e[31mInstallation tools and dependencies\e[0m'
+yum install -y wget make cmake gcc gcc-c++  &&\
+yum install -y pcre-devel lib zlib-devel  \
+    openssl openssl-devel createrepo yum-util
+if [ $? -eq 0 ];then
+    echo -e '\e[32mSuccessful!\e[0m'
+else
+    echo -e '\e[31mFailed\e[0m'
+    exit 0
+fi
+echo -e '\e[31mInstallation nginx\e[0m'
+wget http://nginx.org/download/nginx-1.12.2.tar.gz && \
+tar -xzvf  nginx-1.12.2.tar.gz && rm -f nginx-1.12.2.tar.gz && cd nginx-1.12.2 &&\
+./configure --prefix=/usr/local/nginx && \
+make && make install
+if [ $? -eq 0 ];then
+    echo -e '\e[32mSuccessful!\e[0m'
+else
+    echo -e '\e[31mFailed\e[0m'
+    exit 0
+fi
+echo -e '\e[31mStart nginx\e[0m'
+firewall-cmd --zone=public --permanent --add-port=80/tcp && firewall-cmd --reload
+/usr/local/nginx/sbin/nginx
+echo -e '\e[32mSuccessful!\e[0m'
+
