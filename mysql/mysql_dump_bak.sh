2f5e85b36f0082dade26cf048f5d9e4178f4e335
cyril <36559067@qq.com>
2018-09-04 11:39:27 +0800

    .

--- mysql/mysql_dump_bak.sh
+++ mysql/mysql_dump_bak.sh
@@ -1,21 +1,26 @@
 #!/bin/bash
 #mysql数据库备份
 DATE=$(date +%F_%H-%M-%S)
-HOST=192.168.1.120  #MySQL的主机ip
-DB=test	#数据库名
-USER=bak  #数据库用户名
-PASS=123456	#数据库密码
-MAIL="zhangsan@example.com lisi@example.com"  #收取邮箱的地址
+HOST=172.16.75.251  #MySQL的主机ip
+DB=wordpress    #数据库名
+USER=root  #数据库用户名
+PASS=123456 #数据库密码
+PORT=13333
+MAIL="guoshengye@opstest.cn gsy36559067@163.com"  #收取邮箱的地址
 BACKUP_DIR=/data/db_backup
 SQL_FILE=${DB}_full_$DATE.sql
-BAK_FILE=${DB}_full_$DATE.zip
+BAK_FILE=${DB}_full_$DATE.tar.gz
 cd $BACKUP_DIR
-if mysqldump -h$HOST -u$USER -p$PASS --single-transaction --routines --triggers -B $DB > $SQL_FILE; then
-    zip $BAK_FILE $SQL_FILE && rm -f $SQL_FILE
-    if [ ! -s $BAK_FILE ]; then
-            echo "$DATE 内容" | mail -s "主题" $MAIL
+if mysqldump -h$HOST -u$USER -p$PASS -P$PORT --single-transaction --routines --triggers -B $DB > $SQL_FILE
+then
+    tar -zcvf  $BAK_FILE $SQL_FILE && rm -f $SQL_FILE
+    if [ -s "$BAK_FILE" ]
+    then
+       echo "$DATE 内容" | mail -A exmail -v -s "博客数据库备份成功" $MAIL
+    else
+       echo "$DATE 内容" | mail -A exmail -v -s "博客数据库备份失败" $MAIL
     fi
 else
-    echo "$DATE 内容" | mail -s "主题" $MAIL
+    echo "$DATE 内容" | mail -A exmail -v -s "博客数据库备份失败" $MAIL
 fi
-find $BACKUP_DIR -name '*.zip' -ctime +14 -exec rm {} \;
+find $BACKUP_DIR -name '*.tar.gz' -ctime +14 -exec rm {} \;
---
+++ ss-panel/README.md
@@ -0,0 +1,17 @@
+要求centos 7 x64
+
+ss-panel mod魔改版一键脚本
+
+yum install screen wget -y &&screen -S ss 
+wget -N --no-check-certificate https://raw.githubusercontent.com/mmmwhy/ss-panel-and-ss-py-mu/master/ss-panel-v3-mod.sh && chmod +x ss-panel-v3-mod.sh && bash ss-panel-v3-mod.sh
+
+ss-panel v3一键脚本
+yum install screen wget -y &&screen -S ss
+wget -N --no-check-certificate https://raw.githubusercontent.com/mmmwhy/ss-panel-and-ss-py-mu/master/ss-panel_node.sh && chmod +x ss-panel_node.sh && bash ss-panel_node.sh
+
+选项都差不多,直接输入1安装即可
+
+默认账号：91vps
+
+默认密码：91vps
+
